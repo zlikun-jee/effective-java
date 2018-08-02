f587327a3c0832bcf9d9e2ad0be1a34ae4411645
zlikun <zlikun-dev@hotmail.com>
2018-08-02 17:01:41 +0800

    阅读JDK1.7HashMap源码

---
+++ src/test/java/com/zlikun/jee/j002/HashMapTest.java
@@ -0,0 +1,42 @@
+package com.zlikun.jee.j002;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * HashMap源码解读
+ *
+ * @author zlikun <zlikun-dev@hotmail.com>
+ * @date 2018/8/2 16:01
+ */
+public class HashMapTest {
+
+    @Test
+    public void test() {
+
+        HashMap<String, String> map = new HashMap<>();
+
+        assertTrue(map.isEmpty());
+
+        map.put("name", "zlikun");
+        map.put("name", "Ashe");
+
+        assertEquals(1, map.size());
+
+        assertEquals("Ashe", map.get("name"));
+        assertEquals(null, map.get("age"));
+
+        String value = map.remove("name");
+        assertEquals("Ashe", value);
+
+        assertTrue(map.isEmpty());
+
+        map.clear();
+
+    }
+
+}
