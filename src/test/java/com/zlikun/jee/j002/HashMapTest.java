be5177416f8f90531340966956959935911c8879
zlikun <zlikun-dev@hotmail.com>
2018-08-03 12:31:17 +0800

    测试HashMap在多线程下死循环的问题

--- src/test/java/com/zlikun/jee/j002/HashMapTest.java
+++ src/test/java/com/zlikun/jee/j002/HashMapTest.java
@@ -1,11 +1,14 @@
 package com.zlikun.jee.j002;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * HashMap源码解读
@@ -36,6 +39,40 @@ public class HashMapTest {
         assertTrue(map.isEmpty());
 
         map.clear();
+
+    }
+
+    /**
+     * 多线程下使用HashMap测试，通过多次测试发现在添加阶段（添加阶段会先查找）和查询阶段都可能死循环
+     */
+    @Test @Ignore
+    public void thread() {
+
+        final Map<String, Integer> map = new HashMap<>();
+
+        System.out.println("开始运行测试程序");
+        ExecutorService exec = Executors.newFixedThreadPool(16);
+        for (int i = 0; i < 1000; i++) {
+            final int index = i;
+            exec.execute(new Runnable() {
+                @Override
+                public void run() {
+                    map.put("key-" + index, index);
+                }
+            });
+        }
+
+        exec.shutdown();
+        while (!exec.isTerminated()) ;
+
+        System.out.printf("插入数量量：%d%n", map.size());
+        System.out.println("添加数据完成，开始查询");
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            assertNotNull(entry.getValue());
+        }
+
+        System.out.println("数据查询完成，程序退出");
 
     }
 
